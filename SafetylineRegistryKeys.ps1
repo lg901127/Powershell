04131082073e724c960b40499347b9bcad82be43
lg901127 <lg901127@gmail.com>
2016-03-29 14:35:07 -0700

    Added files via upload

--- SafetylineRegistryKeys.ps1
+++ SafetylineRegistryKeys.ps1
@@ -1,11 +1,12 @@
 ﻿$hostName = hostname
+$SQLServerName = Read-Host -Prompt "Enter the hostname of SQL server"
+$saPassword = Read-Host -Prompt "Enter the password for SQL Server sa user"
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Tsunami Solutions Ltd.\SafetyLine\CompanyDB' -Name DBName -Value SafetyLine
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Tsunami Solutions Ltd.\SafetyLine\CompanyDB' -Name DBPassword -Value safenlog
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Tsunami Solutions Ltd.\SafetyLine\CompanyDB' -Name DBServer -Value VM-SQL3
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Tsunami Solutions Ltd.\SafetyLine\CompanyDB' -Name DBUser -Value sn
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Tsunami Solutions Ltd.\SafetyLine\CompanyDB' -Name DBPassword -Value $saPassword
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Tsunami Solutions Ltd.\SafetyLine\CompanyDB' -Name DBServer -Value $SQLServerName
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Tsunami Solutions Ltd.\SafetyLine\CompanyDB' -Name DBUser -Value sa
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Tsunami Solutions Ltd.\SafetyLine\CompanyDB' -Name DBName -Value SafetyLine
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Tsunami Solutions Ltd.\SafetyLine\CompanyDB' -Name DBPassword -Value safenlog
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Tsunami Solutions Ltd.\SafetyLine\CompanyDB' -Name DBServer -Value VM-SQL3
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Tsunami Solutions Ltd.\SafetyLine\CompanyDB' -Name DBUser -Value sn
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Tsunami Solutions Ltd.\SafetyLine' -Name Name -Value $hostName
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Tsunami Solutions Ltd.\SafetyLine\Pusher' -Name AppUrl -Value 'www.slmonitor.com'
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Tsunami Solutions Ltd.\SafetyLine\CompanyDB' -Name DBPassword -Value $saPassword
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Tsunami Solutions Ltd.\SafetyLine\CompanyDB' -Name DBServer -Value $SQLServerName
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Tsunami Solutions Ltd.\SafetyLine\CompanyDB' -Name DBUser -Value sa
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Tsunami Solutions Ltd.\Safet
