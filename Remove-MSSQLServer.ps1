285bb2a0066dd161abe9f90de3a78230b17f3bb1
lg901127 <lg901127@gmail.com>
2016-03-29 16:12:55 -0700

    Added files via upload

---
+++ Remove-MSSQLServer.ps1
@@ -0,0 +1,20 @@
+﻿function Remove-MSSQLServer ([string]$arg) {
+    #Read in specific value of a registry key:
+    $v = Get-ItemProperty -Path $arg -Name DependOnService 
+    #Transfer the value to System Collections ArrayList to make it a editable array:
+    $v_new = [System.Collections.ArrayList](Select-Object -InputObject $v -ExpandProperty DependOnService)
+    #Remove specific value from array:
+    $v_new.Remove('MSSQLServer')
+    #Set the value in registry with the new array:
+    Set-ItemProperty -Path $arg -Name DependOnService -Value $v_new
+
+}
+
+
+$SafetyLineMaintenance = 'HKLM:\SYSTEM\CurrentControlSet\Services\SafetyLineMaintenance'
+$SafetyLinePusher = 'HKLM:\SYSTEM\CurrentControlSet\Services\SafetyLinePusher'
+$SafetyLineSatelliteEmailService = 'HKLM:\SYSTEM\CurrentControlSet\Services\SatelliteEmailService'
+
+Remove-MSSQLServer($SafetyLineMaintenance)
+Remove-MSSQLServer($SafetyLinePusher)
+Remove-MSSQLServer($SafetyLineSatelliteEmailService)
